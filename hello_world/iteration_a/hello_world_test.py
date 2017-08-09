4e349c913f5fa78b938eff6273b58a352f67dc54
Justin Beall <jus.beall@gmail.com>
2017-08-09 00:21:45 -0400

    hello_world-a: Add failing test with mock builtin.print

--- hello_world/iteration_a/hello_world_test.py
+++ hello_world/iteration_a/hello_world_test.py
@@ -1,1 +1,7 @@
-import hello_world.iteration_a.hello_world
+from unittest.mock import patch
+
+
+@patch('builtins.print')
+def test_hello_world(mock_print):
+    import hello_world.iteration_a.hello_world
+    mock_print.assert_called_with('hello world')
