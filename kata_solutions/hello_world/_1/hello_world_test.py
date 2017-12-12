ed3c31bcc4f849b027a84efd58b5ac662e3f5cf2
Justin Beall <jus.beall@gmail.com>
2017-12-11 22:25:56 -0500

    Refactor solutions directory layout

--- kata_solutions/hello_world/_1/hello_world_test.py
+++ kata_solutions/hello_world/_1/hello_world_test.py
@@ -3,4 +3,5 @@ from unittest.mock import patch
 
 @patch('builtins.print')
 def test_hello_world(mock_print):
+    import kata_solutions.hello_world._1.hello_world
     mock_print.assert_called_with('hello world')
