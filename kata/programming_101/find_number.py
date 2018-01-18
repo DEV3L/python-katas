7e89f6aaa038f0129638dfae24bd523502908f94
Justin Beall <jus.beall@gmail.com>
2018-01-18 13:00:02 -0500

    Add Programming 101

---
+++ kata/programming_101/find_number.py
@@ -0,0 +1,25 @@
+number_str = '123456789'
+
+
+def find_number(search):
+    if search in number_str:
+        print('hello')
+    else:
+        print('goodbye')
+
+
+find_number('0')
+#
+#
+# if True:
+#     print('hello world')
+# if False:
+#     print('goodbye world')
+
+# integer => int => whole number
+# float => float => decimal
+# boolean => bool => True/False
+# string => str => 'word'
+#
+#
+# print(my_variable)
