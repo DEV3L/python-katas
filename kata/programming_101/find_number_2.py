891d8bff50eccd725b9cf85f83a6a1e6855cca71
Justin Beall <jus.beall@gmail.com>
2018-02-01 12:40:30 -0500

    Added casting examples

--- kata/programming_101/find_number_2.py
+++ kata/programming_101/find_number_2.py
@@ -1,15 +1,60 @@
 # DATA TYPES
 
-# str
+# str - string
 _str = 'test'
 _str2 = "tst"
 _str3 = '{_str2} test'.format(_str2=_str2)
 _str4 = f'{_str} test'
 
-# bool
+# bool - boolean
 _bool = True
 _bool_false = False
 
-# int
+# int - integer
 _int = 1
 _int_negative = -1
+
+# float
+_float = 1.0
+_float_negative = -1.0
+
+# list
+
+_list = ['alex', 'grande', 'sara',
+         'danie', 'laura', 'jen']
+
+for name in _list:
+    print(name)
+
+_list2 = list()
+
+# CASTING
+
+output = float('1.0') + 1
+output2 = '1' + str(1)
+
+print(output)
+print(output2)
+
+print(bool(0))
+print(bool(1))
+print(bool(.1))
+print('---')
+
+print(len(''))
+print(len('false'))
+
+print(bool(''))
+print(bool('false'))
+print(bool(None))
+print(bool([]))
+
+"""
+  practice: print each letter in a given string
+"""
+
+# for {variable_name} in <collection>:
+#     <action>
+name = 'name'
+for character in name:
+    print(character)
