1fba94c73ee8c8741c486393036670c894ca9c4d
Justin Beall <jus.beall@gmail.com>
2018-02-01 12:22:40 -0500

    Add more Python datatype katas

---
+++ kata/programming_101/find_number_2.py
@@ -0,0 +1,15 @@
+# DATA TYPES
+
+# str
+_str = 'test'
+_str2 = "tst"
+_str3 = '{_str2} test'.format(_str2=_str2)
+_str4 = f'{_str} test'
+
+# bool
+_bool = True
+_bool_false = False
+
+# int
+_int = 1
+_int_negative = -1
