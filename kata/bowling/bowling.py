be817b84ea9abc1e538564c87f49ce42bd25bdeb
Justin Beall <jus.beall@gmail.com>
2018-02-11 23:17:23 -0500

    Add first spare calculation

--- kata/bowling/bowling.py
+++ kata/bowling/bowling.py
@@ -1,5 +1,15 @@
 def score_game(frames: list):
     score = 0
-    for frame in frames:
+
+    for round_number, frame in enumerate(frames):
+        is_spare = False
+
         score += sum(frame)
+
+        if score == 10:
+            is_spare = True
+
+        if is_spare and round_number < 9:
+            score += frames[round_number + 1][0]
+
     return score
--- kata/bowling/bowling_test.py
+++ kata/bowling/bowling_test.py
@@ -25,6 +25,8 @@ def _frames():
 @mark.parametrize('expected_score, scores', [
     (0, []),
     (1, [(1, 0)]),
+    (10, [(1, 9)]),
+    (10, [(10, 0)]),
     (10, list(itertools.repeat((1, 0), 10))),
     (30, list(itertools.repeat((1, 2), 10)))
 ])
@@ -36,6 +38,17 @@ def test_score(expected_score, scores, frames):
     assert expected_score == score
 
 
+def test_spare(frames):
+    # If in two tries he knocks them all down, this is called a “spare”
+    #  and his score for the frame is ten plus the number of pins knocked down on his next throw (in his next turn).
+    frames[0] = (1, 9)
+    frames[1] = (1, 0)
+    expected_score = 12
+
+    score = score_game(frames)
+
+    assert expected_score == score
+
 """
 Bowling Kata
 Based upon Uncle Bob's Adeventure's in C# Bowling Game
@@ -45,19 +58,6 @@ Problem Description
 Create a program, which, given a valid sequence of rolls for one line of American Ten-Pin Bowling,
 produces the total score for the game. Here are some things that the program will not do:
 
-We will not check for valid rolls.
-We will not check for correct number of rolls and frames.
-We will not provide scores for intermediate frames.
-Depending on the application, this might or might not be a valid way to define a complete story, but we do it here for purposes of keeping the kata light. I think you’ll see that improvements like those above would go in readily if they were needed for real.
-We can briefly summarize the scoring for this form of bowling:
-
-Each game, or “line” of bowling, includes ten turns, or “frames” for the bowler.
-
-In each frame, the bowler gets up to two tries to knock down all the pins.
-
-If in two tries, he fails to knock them all down, his score for that frame is the total number of pins knocked down in his two tries.
-
-If in two tries he knocks them all down, this is called a “spare” and his score for the frame is ten plus the number of pins knocked down on his next throw (in his next turn).
 
 If on his first try in the frame he knocks down all the pins, this is called a “strike”. His turn is over, and his score for the frame is ten plus the simple total of the pins knocked down in his next two rolls.
 
