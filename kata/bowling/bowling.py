b1f3908b825e4c980965a5efc483f83a69aabf12
Justin Beall <jus.beall@gmail.com>
2018-02-11 23:38:19 -0500

    Add first strike implementation

--- kata/bowling/bowling.py
+++ kata/bowling/bowling.py
@@ -3,14 +3,28 @@ def score_game(frames: list):
 
     for round_number, frame in enumerate(frames):
         is_spare = False
+        is_strike = False
 
         round_score = sum(frame)
         score += round_score
 
         if round_score == 10:
             is_spare = True
+            if frame[0] == 10:
+                is_strike = True
 
-        if is_spare and round_number < 9:
-            score += frames[round_number + 1][0]
+        next_roll = frames[round_number + 1][0] if round_number < 9 else 0
+
+        if is_strike:
+            if next_roll == 10:
+                second_roll = frames[round_number + 1][0] if round_number < 8 else 0
+            else:
+                second_roll = frames[round_number + 1][1] if round_number < 9 else 0
+
+            score += second_roll
+
+        if is_spare:
+            score += next_roll
+
 
     return score
--- kata/bowling/bowling_test.py
+++ kata/bowling/bowling_test.py
@@ -39,6 +39,33 @@ def test_spare(expected_score, scores, frames):
     assert expected_score == score
 
 
+def test_strike_one_zero(frames):
+    # If on his first try in the frame he knocks down all the pins, this is called a “strike”.
+    #  His turn is over, and his score for the frame is ten plus the simple total of the pins
+    #  knocked down in his next two rolls.
+
+    frames[0] = (10, 0)
+    frames[1] = (1, 0)
+
+    expected_value = 12
+    score = score_game(frames)
+
+    assert expected_value == score
+
+
+def test_strike_one_one(frames):
+    # If on his first try in the frame he knocks down all the pins, this is called a “strike”.
+    #  His turn is over, and his score for the frame is ten plus the simple total of the pins
+    #  knocked down in his next two rolls.
+
+    frames[0] = (10, 0)
+    frames[1] = (1, 1)
+
+    expected_value = 14
+    score = score_game(frames)
+
+    assert expected_value == score
+
 def _substitute_frame_scores(frames, scores):
     for index, score in enumerate(scores):
         frames[index] = score
@@ -57,8 +84,6 @@ def _substitute_frame_scores(frames, scores):
 Create a program, which, given a valid sequence of rolls for one line of American Ten-Pin Bowling,
 produces the total score for the game. Here are some things that the program will not do:
 
-
-If on his first try in the frame he knocks down all the pins, this is called a “strike”. His turn is over, and his score for the frame is ten plus the simple total of the pins knocked down in his next two rolls.
 
 If he gets a spare or strike in the last (tenth) frame, the bowler gets to throw one or two more bonus balls, respectively. These bonus throws are taken as part of the same turn. If the bonus throws knock down all the pins, the process does not repeat: the bonus throws are only used to calculate the score of the final frame.
 
