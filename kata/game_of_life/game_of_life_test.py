550d15ecbf9910251b9729181b4ce99cee4c6295
Justin Beall <jus.beall@gmail.com>
2018-02-20 23:32:58 -0500

    Extract build board from game of life kata test

--- kata/game_of_life/game_of_life_test.py
+++ kata/game_of_life/game_of_life_test.py
@@ -1,16 +1,11 @@
+import itertools
+
 from kata.game_of_life.game_of_life import game_of_life
 
 
 def test_game_of_life_zero_board():
-    expected_next_generation = \
-        'Generation 2:\n' \
-        '0 0\n' \
-        ''
-
-    generation_input = \
-        'Generation 1:\n' \
-        '0 0\n' \
-        ''
+    expected_next_generation = _build_board(2, (0, 0), ())
+    generation_input = _build_board(1, (1, 1), ())
 
     next_generation = game_of_life(generation_input)
 
@@ -18,15 +13,8 @@ def test_game_of_life_zero_board():
 
 
 def test_game_of_life_one_empty_board():
-    expected_next_generation = \
-        'Generation 2:\n' \
-        '1 1\n' \
-        '.'
-
-    generation_input = \
-        'Generation 1:\n' \
-        '1 1\n' \
-        '.'
+    expected_next_generation = _build_board(2, (1, 1), ())
+    generation_input = _build_board(1, (1, 1), ())
 
     next_generation = game_of_life(generation_input)
 
@@ -34,11 +22,7 @@ def test_game_of_life_one_empty_board():
 
 
 def test_game_of_life_one_live_board():
-    expected_next_generation = \
-        'Generation 2:\n' \
-        '1 1\n' \
-        '.'
-
+    expected_next_generation = _build_board(2, (1, 1), ())
     generation_input = \
         'Generation 1:\n' \
         '1 1\n' \
@@ -47,3 +31,15 @@ def test_game_of_life_one_live_board():
     next_generation = game_of_life(generation_input)
 
     assert expected_next_generation == next_generation
+
+
+def _build_board(generation_number: int, board_size: tuple, live_nodes: tuple):
+    return_generation = f'Generation {generation_number}:\n'
+    return_generation += f'{board_size[0]} {board_size[1]}\n'
+
+    board_line = ''.join(list(itertools.repeat('.', board_size[0])))
+    board = '\n'.join(list(itertools.repeat(board_line, board_size[1])))
+
+    return_generation += board
+
+    return return_generation
