d5be665f63e1f2b6a225fe3e03339f54fdefa61e
Justin Beall <jus.beall@gmail.com>
2018-02-20 23:21:17 -0500

    Game of life kata - one

--- kata/game_of_life/game_of_life.py
+++ kata/game_of_life/game_of_life.py
@@ -1,2 +1,15 @@
 def game_of_life(input_board):
-    return ''
+    input_board_lines = input_board.split('\n')
+
+    generation_header = input_board_lines[0].replace('1', '2')
+    board_size = input_board_lines[1]
+    board = input_board_lines[2:]
+
+    if board_size != '0 0':
+        for index, row in enumerate(board):
+            board[index] = '.'
+
+    output = [generation_header, board_size]
+    output.extend(board)
+
+    return '\n'.join(output)
--- kata/game_of_life/game_of_life_test.py
+++ kata/game_of_life/game_of_life_test.py
@@ -1,13 +1,48 @@
 from kata.game_of_life.game_of_life import game_of_life
 
 
-def test_game_of_life_empty_board():
-    expected_next_generation = ''
+def test_game_of_life_zero_board():
+    expected_next_generation = \
+        'Generation 2:\n' \
+        '0 0\n' \
+        ''
 
     generation_input = \
         'Generation 1:\n' \
         '0 0\n' \
         ''
+
+    next_generation = game_of_life(generation_input)
+
+    assert expected_next_generation == next_generation
+
+
+def test_game_of_life_one_empty_board():
+    expected_next_generation = \
+        'Generation 2:\n' \
+        '1 1\n' \
+        '.'
+
+    generation_input = \
+        'Generation 1:\n' \
+        '1 1\n' \
+        '.'
+
+    next_generation = game_of_life(generation_input)
+
+    assert expected_next_generation == next_generation
+
+
+def test_game_of_life_one_live_board():
+    expected_next_generation = \
+        'Generation 2:\n' \
+        '1 1\n' \
+        '.'
+
+    generation_input = \
+        'Generation 1:\n' \
+        '1 1\n' \
+        '*'
 
     next_generation = game_of_life(generation_input)
 
