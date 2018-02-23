7583a1376fa4692bba6e104432dca1b15ae2ca5e
Justin Beall <jus.beall@gmail.com>
2018-02-23 14:03:23 -0500

    Add programming 101 work

--- kata/programming_101/seach_string.py
+++ kata/programming_101/seach_string.py
@@ -8,12 +8,44 @@
 # list
 # dict
 
+attendees = ['sara', 'alex', 'justin', 'ryan']
+
+for attendee in attendees:
+    # print(attendee)
+    pass
+
+# print(attendees[0])
+
+
+# key = value
+employees = {
+    'sara': 'csa',
+    'alex': 'it stupport tech',
+    'justin': 'software ninja',
+    'ryan': 'numbers nerd',
+    'robot': str(1)
+}
+
+# print(employees['alex'])
+# print(employees.get('ilya', 'russian spy'))
+
+# for employee_id in employees:
+#     print(employee_id + ' - ' + employees[employee_id])
+
+# for key, value in employees.items():
+#     print(key + ' - ' + value)
+
+
 # CASTING
 # str + int = runtime exception
 
 # SCOPE
 # white space in Python defines scope
 #     block of code associated with a control structure
+
+# def my_method():
+#     temp = 1
+# print(temp)
 
 # CONTROL STRUCTURES
 # --for loops--
@@ -27,6 +59,22 @@
 #   pass
 # else <bool>:
 #   pass
+
+# -- exception handling --
+# try <expression>:
+#   <action>
+# except [error_type]:
+#   <handle error>
+
+# try:
+#     employees['iyla']
+# except KeyError:
+#     print('call alex to add access')
+#     print('in exception')
+# except Exception:
+#     print('here')
+# else:
+#     print('else')
 
 # --assignment--
 # =
@@ -44,10 +92,23 @@
   PRACTICE: print each letter in a given string
 """
 
+name = 'justin'
+
+# for char in name:
+#     print(char)
+
 """
   PRACTICE: create a function that takes an input,
   then prints each character of the input
 """
+
+
+def print_char(input_name):
+    for char in input_name:
+        print(char)
+
+
+# print_char(name)
 
 
 """
@@ -57,6 +118,12 @@
 """
 
 text_value = 'some input'
-# print(search_string('a', text_value))  # False
-# print(search_string('s', text_value))  # True
-# print(search_string('S', text_value))  # False
+
+
+def search_string(search_value, text_value):
+    return search_value in text_value
+
+
+print(search_string('a', text_value))  # False
+print(search_string('s', text_value))  # True
+print(search_string('S', text_value))  # False
