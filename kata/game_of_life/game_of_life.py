f311c5786c83eaf916a6cb6df58c4ba880448ba0
Justin Beall <jus.beall@gmail.com>
2018-02-25 18:18:10 -0500

    Refactor game of life solution

--- kata/game_of_life/game_of_life.py
+++ kata/game_of_life/game_of_life.py
@@ -1,48 +1,52 @@
+class Game:
+    DEAD_CELL = '.'
+    LIVE_CELL = '*'
+
+    def __init__(self, input_game_data):
+        self._input_game_data_lines = input_game_data.split('\n')
+        self._live_cells = None
+
+    @property
+    def next_generation_header(self):
+        return self._input_game_data_lines[0].replace('1', '2')
+
+    @property
+    def board_size(self):
+        return self._input_game_data_lines[1]
+
+    @property
+    def start_state(self):
+        return self._input_game_data_lines[2:]
+
+    def find_live_cells(self):
+        if self._live_cells:
+            return self._live_cells
+
+        self._live_cells = []
+        for x, game_row in enumerate(self.start_state):
+            for y, cell in enumerate(game_row):
+                if cell == self.LIVE_CELL:
+                    self._live_cells.append((x, y))
+
+        return self._live_cells
+
+    def build_empty_board(self):
+        board_dimensions = self.board_size.split(' ')
+        empty_board = [[self.DEAD_CELL for x in range(int(board_dimensions[1]))] for y in
+                       range(int(board_dimensions[0]))]
+        return empty_board
+
 def game_of_life(input_board):
-    input_board_lines = input_board.split('\n')
+    game = Game(input_board)
 
-    generation_header = input_board_lines[0].replace('1', '2')
-    board_size = input_board_lines[1]
-
-    if '0 0' == board_size:
-        output = '\n'.join([generation_header, board_size]) + '\n'
-        return output
-
-    start_state = input_board_lines[2:]
-    live_cells = []
-    # find life
-    for x, game_row in enumerate(start_state):
-        for y, cell in enumerate(game_row):
-            if cell == '*':
-                live_cells.append((x, y))
-
-    board_dimensions = board_size.split(' ')
-    board = [['.' for x in range(int(board_dimensions[1]))] for y in range(int(board_dimensions[0]))]
+    live_cells = game.find_live_cells()
+    board = game.build_empty_board()
 
     for x, board_row in enumerate(board):
         for y, board_cell in enumerate(board_row):
             is_live = (x, y) in live_cells
 
-            # count neighbors
-            # xxx -> (x-1, y-1), (x, y-1), (x+1, y-1)
-            # xox -> (x-1, y), (SELF), (x+1, y)
-            # xxx -> (x-1, y+1), (x, y+1), (x+1, y+1)
-            neighbor_count = 0
-
-            # top row
-            neighbor_count += is_live_cell(live_cells, x - 1, y - 1)
-            neighbor_count += is_live_cell(live_cells, x, y - 1)
-            neighbor_count += is_live_cell(live_cells, x + 1, y - 1)
-
-            # middle
-            neighbor_count += is_live_cell(live_cells, x - 1, y)
-            # Skip SELF neighbor_count += is_live_cell(board, x, y)
-            neighbor_count += is_live_cell(live_cells, x + 1, y)
-
-            # bottom
-            neighbor_count += is_live_cell(live_cells, x - 1, y + 1)
-            neighbor_count += is_live_cell(live_cells, x, y + 1)
-            neighbor_count += is_live_cell(live_cells, x + 1, y + 1)
+            neighbor_count = _count_neighbors(live_cells, x, y)
 
             if not is_live and neighbor_count == 3:
                 is_live = True
@@ -53,7 +57,7 @@ def game_of_life(input_board):
 
             board[x][y] = '*' if is_live else '.'
 
-    output = [generation_header, board_size]
+    output = [game.next_generation_header, game.board_size]
 
     # for row in board:
     board_str = ''
@@ -67,6 +71,28 @@ def game_of_life(input_board):
     output.append(board_str)
 
     return '\n'.join(output)
+
+
+def _count_neighbors(live_cells, x, y):
+    # count neighbors
+    # xxx -> (x-1, y-1), (x, y-1), (x+1, y-1)
+    # xox -> (x-1, y), (SELF), (x+1, y)
+    # xxx -> (x-1, y+1), (x, y+1), (x+1, y+1)
+    neighbor_count = 0
+    # top row
+    neighbor_count += is_live_cell(live_cells, x - 1, y - 1)
+    neighbor_count += is_live_cell(live_cells, x, y - 1)
+    neighbor_count += is_live_cell(live_cells, x + 1, y - 1)
+    # middle
+    neighbor_count += is_live_cell(live_cells, x - 1, y)
+    # Skip SELF neighbor_count += is_live_cell(board, x, y)
+    neighbor_count += is_live_cell(live_cells, x + 1, y)
+    # bottom
+    neighbor_count += is_live_cell(live_cells, x - 1, y + 1)
+    neighbor_count += is_live_cell(live_cells, x, y + 1)
+    neighbor_count += is_live_cell(live_cells, x + 1, y + 1)
+
+    return neighbor_count
 
 
 def is_live_cell(live_cells, x, y):
