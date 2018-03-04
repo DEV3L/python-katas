21f0139add55fd3f97e6bb7ca5c8a051ed65ff7a
Justin Beall <jus.beall@gmail.com>
2018-03-04 01:47:01 -0500

    Add seconds and minutes calculation

--- kata/parse_string_time/parse_string_time.py
+++ kata/parse_string_time/parse_string_time.py
@@ -1,2 +1,18 @@
 def parse_string_time(input_time: str) -> float:
-    return 1
+    try:
+        return _parse_string_time(input_time)
+    except:
+        return 0
+
+
+def _parse_string_time(input_time: str) -> float:
+    amount = int(input_time.split(' ')[0])
+    duration_type = input_time.split(' ')[1]
+
+    multiplier = 0
+    if 'min' == duration_type:
+        multiplier = 60
+    elif 'sec' == duration_type:
+        multiplier = 1
+
+    return amount * multiplier
--- kata/parse_string_time/parse_string_time_test.py
+++ kata/parse_string_time/parse_string_time_test.py
@@ -2,14 +2,11 @@
 
 """
 |'1 ms'  | .001 |
-|'1 sec' | 1 |
-|'1 min' | 60 |
 |'1 hr'  | 3600 |
 |'5 sec 421 ms' | 5.421 |
 |'9 min 57 sec' | 597 |
 |'1 hr 33 min'  | 5580 |
 |'1 hr 1 min 1 sec 111 ms' | 3661.111 |
-|'' | 0 |
 """
 from pytest import mark
 
@@ -17,8 +14,20 @@ from kata.parse_string_time.parse_string_time import parse_string_time
 
 
 @mark.parametrize('input_value, expected_value', [
-    ('1 sec', 1)
+    # seconds
+    ('1 sec', 1),
+    ('2 sec', 2),
+    ('3 sec', 3),
+    # minutes
+    ('1 min', 60),
+    ('3 min', 180),
+    # invalid
+    ('x sec', 0),
+    ('', 0),
+    ('1 se', 0),
+    ('10 minute', 0),
+    ('5', 0)
 ])
-def test_seconds(input_value, expected_value):
+def test_parse_string_time(input_value, expected_value):
     time_value = parse_string_time(input_value)
     assert expected_value == time_value
