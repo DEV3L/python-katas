0b1aba89987d0274e67ee4c5b6f3f0061f711294
Justin Beall <jus.beall@gmail.com>
2018-03-04 21:43:47 -0500

    Complete parse string time

--- kata/parse_string_time/parse_string_time.py
+++ kata/parse_string_time/parse_string_time.py
@@ -1,14 +1,17 @@
 def parse_string_time(input_time: str) -> float:
-    try:
-        return _parse_string_time(input_time)
-    except:
-        return 0
+    total_amount = 0
+
+    times = _slice_input_times(input_time)
+
+    for _amount, duration_type in times:
+        amount = _to_float(_amount)
+        multiplier = _parse_multiplier(duration_type)
+        total_amount += amount * multiplier
+
+    return total_amount
 
 
-def _parse_string_time(input_time: str) -> float:
-    amount = int(input_time.split(' ')[0])
-    duration_type = input_time.split(' ')[1]
-
+def _parse_multiplier(duration_type):
     multiplier = 0
     if 'ms' == duration_type:
         multiplier = .001
@@ -18,5 +21,20 @@ def _parse_string_time(input_time: str) -> float:
         multiplier = 60
     elif 'hr' == duration_type:
         multiplier = 60
+    return multiplier
 
-    return amount * multiplier
+
+def _slice_input_times(input_time: str) -> iter:
+    input_time_chunks = iter(input_time.split(' '))
+    input_time_tuples = zip(input_time_chunks, input_time_chunks)
+
+    return input_time_tuples
+
+
+def _to_float(amount: str) -> float:
+    try:
+        amount = float(amount)
+    except:
+        amount = 0.0
+
+    return amount
--- kata/parse_string_time/parse_string_time_test.py
+++ kata/parse_string_time/parse_string_time_test.py
@@ -1,13 +1,3 @@
-# ZOMBIES
-
-"""
-|'1 ms'  | .001 |
-|'1 hr'  | 3600 |
-|'5 sec 421 ms' | 5.421 |
-|'9 min 57 sec' | 597 |
-|'1 hr 33 min'  | 5580 |
-|'1 hr 1 min 1 sec 111 ms' | 3661.111 |
-"""
 from pytest import mark
 
 from kata.parse_string_time.parse_string_time import parse_string_time
@@ -30,6 +20,7 @@ from kata.parse_string_time.parse_string_time import parse_string_time
     ('3 hr', 180),
     # joined
     ('3 hr 5 sec', 185),
+    ('1 hr 1 ms', 60.001),
     # invalid
     ('x sec', 0),
     ('', 0),
