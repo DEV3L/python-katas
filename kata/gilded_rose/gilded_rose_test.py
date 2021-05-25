8a0616a8f45745185333622fa8b9b0bd9372adfe
Justin Beall <jus.beall@gmail.com>
2021-05-24 22:37:55 -0400

    Add Gilded Rose kata

---
+++ kata/gilded_rose/gilded_rose_test.py
@@ -0,0 +1,26 @@
+from katas.gilded_rose.gilded_rose import GildedRose
+
+"""
+Z - Zero x
+O - One x
+M - Many x
+B - Boundary Behaviors
+I - Interface Definition x
+E - Exercise Exceptional x
+S - Simple Scenarios, Simple Solutions
+"""
+
+"""
+TDD : Test Driven Development
+
+1. You are not allowed to write any production code unless it is to make a failing unit test pass.
+2. You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
+3. You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
+"""
+
+
+def test_zero():
+    gilded_rose = GildedRose.update_quality([])
+
+    assert len(gilded_rose) == 0
+    assert gilded_rose is not None
